0b8ccafed6e6ea69e7fd91559fe8f65e75fc3dcd
Ivan Vergés <ivan@platoniq.net>
2021-03-31 12:53:50 +0200

    move sentry to an initializer

--- config/application.rb
+++ config/application.rb
@@ -19,11 +19,3 @@ module DecidimPemb
     # the framework and any gems in your application.
   end
 end
-
-if ENV["SENTRY_DSN"]
-  Sentry.init do |config|
-    config.dsn = ENV["SENTRY_DSN"]
-    config.breadcrumbs_logger = [:active_support_logger]
-    config.traces_sample_rate = 0.5
-  end
-end
