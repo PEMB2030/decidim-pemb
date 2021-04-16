303addf864ac50deed3f44aff0742d2560947868
Vera Rojman <vrojman@protonmail.com>
2021-04-16 12:57:01 +0200

    Update translations and fix rubocop offenses

--- db/migrate/20210416102041_create_decidim_access_codes_access_codes.decidim_access_codes_verification.rb
+++ db/migrate/20210416102041_create_decidim_access_codes_access_codes.decidim_access_codes_verification.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 # This migration comes from decidim_access_codes_verification (originally 20210409162973)
 
 class CreateDecidimAccessCodesAccessCodes < ActiveRecord::Migration[5.2]
