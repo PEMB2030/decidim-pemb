f3bc2b2130b7743fe8ae5c916ed827dbb8233042
Vera Rojman <vrojman@protonmail.com>
2021-04-16 12:34:14 +0200

    Replace access requests module with access codes

--- config/initializers/decidim_verifications.rb
+++ config/initializers/decidim_verifications.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-Decidim::Verifications.register_workflow(:actors) do |workflow|
-  workflow.engine = Decidim::AccessRequests::Verification::Engine
-  workflow.admin_engine = Decidim::AccessRequests::Verification::AdminEngine
+Decidim::Verifications.register_workflow(:access_codes) do |workflow|
+  workflow.engine = Decidim::AccessCodes::Verification::Engine
+  workflow.admin_engine = Decidim::AccessCodes::Verification::AdminEngine
   workflow.renewable = true
   workflow.time_between_renewals = 5.minutes
 end
