88389a12c74e2df3f78cfb194587e9ccbed92fcf
Ivan Vergés <ivan@platoniq.net>
2022-02-25 12:56:02 +0100

    fix ssl for test

--- config/initializers/decidim.rb
+++ config/initializers/decidim.rb
@@ -30,7 +30,7 @@ Decidim.configure do |config|
   # config.content_processors = []
 
   # Whether SSL should be enabled or not.
-  config.force_ssl = true
+  config.force_ssl = !Rails.env.starts_with?("test")
 
   # Map and Geocoder configuration
   #
