79b8faae801d96c2c66c246eccf004e6b1364df1
Ivan Vergés <ivan@pokecode.net>
2023-04-28 11:25:04 +0200

    add missing awesome migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_24_142037) do
+ActiveRecord::Schema.define(version: 2023_04_28_091647) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "ltree"
@@ -298,7 +298,7 @@ ActiveRecord::Schema.define(version: 2022_02_24_142037) do
   end
 
   create_table "decidim_awesome_config", force: :cascade do |t|
-    t.jsonb "var"
+    t.string "var"
     t.jsonb "value"
     t.integer "decidim_organization_id"
     t.datetime "created_at", null: false
