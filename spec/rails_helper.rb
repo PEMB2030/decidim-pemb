d0bd1c71eace73cc7813bae277f472ab6b54cb5e
Anna Topalidi <t.opalidisanna@gmail.com>
2024-01-02 13:37:31 +0100

    remove extra code

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -72,22 +72,3 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 end
-require 'selenium/webdriver'
-
-Capybara.register_driver :headless_chrome do |app|
-  options = Selenium::WebDriver::Chrome::Options.new
-  options.args << "--explicitly-allowed-ports=#{Capybara.server_port}"
-  options.args << "--headless-new"
-  options.args << "--no-sandbox"
-  options.args << if ENV["BIG_SCREEN_SIZE"].present?
-                    "--window-size=1920,3000"
-                  else
-                    "--window-size=1920,1080"
-                  end
-  options.args << "--ignore-certificate-errors" if ENV["TEST_SSL"]
-  Capybara::Selenium::Driver.new(
-    app,
-    browser: :chrome,
-    options: options
-  )
-end
