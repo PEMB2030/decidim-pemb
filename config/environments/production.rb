8e48b2f331d8d62d167c20d178f1f3b4f5f7e843
Anna Topalidi <t.opalidisanna@gmail.com>
2024-01-02 13:43:56 +0100

    change config.log_level

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,7 +47,7 @@ Rails.application.configure do
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
-  config.log_level = %w(debug info warn error fatal).include?(ENV.fetch("RAILS_LOG_LEVEL")) ? ENV["RAILS_LOG_LEVEL"] : :info
+  config.log_level = %w(debug info warn error fatal).include?(ENV.fetch("RAILS_LOG_LEVEL", "info")) ? ENV.fetch("RAILS_LOG_LEVEL", "info").to_sym : :info
 
   # Prepend all log lines with the following tags.
   config.log_tags = [:request_id]
